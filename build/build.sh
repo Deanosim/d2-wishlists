bc8a5585a0413a455d7fb16084a32706c76ecfa4
Deanosim <4834976+Deanosim@users.noreply.github.com>
2021-06-06 18:28:37 +1000

    Create build.sh

---
+++ build/build.sh
@@ -0,0 +1,12 @@
+echo "Build and Merge script by Deanosim"
+echo "Download wishlist links from wishlist-links file."
+xargs -n 1 curl -O < wishlist-links
+
+echo "Combine all wishlists into one output.txt"
+cat Season%2014%20Community%20Favs choosy_voltron.txt > input.txt
+
+echo "Remove all duplicate lines"
+awk '!a[$0]++' input.txt > output.txt
+
+echo "cleanup"
+rm Season%2014%20Community%20Favs choosy_voltron.txt
