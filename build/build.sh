cbd382cb2d448a583e7872203a881dff8aae3ce3
Deanosim <4834976+Deanosim@users.noreply.github.com>
2021-09-06 00:18:01 +1000

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -3,11 +3,12 @@ echo "Download wishlist links from wishlist-links file."
 xargs -n 1 curl -O < wishlist-links
 
 echo "Combine all wishlists into one output.txt"
-cat u_Ken-as-fuck%20Season%20of%20the%20Lost%20Wishlist.txt choosy_voltron.txt > input.txt
+cat choosy_voltron.txt u_Ken-as-fuck_SotL_Wishlist.txt > input.txt
 
 echo "Remove all duplicate lines"
 awk '!a[$0]++' input.txt > output.txt
 
 echo "cleanup"
-rm u_Ken-as-fuck%20Season%20of%20the%20Lost%20Wishlist.txt choosy_voltron.txt
+rm u_Ken-as-fuck_SotL_Wishlist.txt choosy_voltron.txt input.txt
 cp output.txt ../master.txt
+rm output.txt
