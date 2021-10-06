1b249ebe4a113ccb2479c313874a07974ed8e553
Deanosim <4834976+Deanosim@users.noreply.github.com>
2021-10-06 23:54:29 +1100

    Update Wishlist and fix build script.

--- build/build.sh
+++ build/build.sh
@@ -3,13 +3,13 @@ echo "Download wishlist links from wishlist-links file."
 xargs -n 1 curl -O < wishlist-links
 
 echo "Combine all wishlists into one output.txt"
-cat choosy_voltron.txt u_Ken-as-fuck_SotL_Wishlist.txt > input.txt
+cat choosy_voltron.txt > input.txt
 
 echo "Remove all duplicate lines"
 #awk '!a[$0]++' input.txt > output.txt
 cp input.txt output.txt
 
 echo "cleanup"
-rm u_Ken-as-fuck_SotL_Wishlist.txt choosy_voltron.txt input.txt
+rm choosy_voltron.txt input.txt
 cp output.txt ../dim-master.txt
 rm output.txt
